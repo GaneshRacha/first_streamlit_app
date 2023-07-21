90bee6c9f00faad1b43759e03ff6ac399d04a753
GaneshRacha <60355360+GaneshRacha@users.noreply.github.com>
2023-07-21 18:04:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+streamlit.stop()
+
 import snowflake.connector
-
-
 
 
 # write your own comment -what does the next line do? 
